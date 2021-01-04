c7e896a3b25bfd21d7280524bf1597fc9b417d9c
unknown <joksimovicmiki7@gmail.com>
2021-01-04 14:17:27 +0100

    Product and Store Factories

---
+++ database/factories/StoreFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Store;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class StoreFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Store::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name . "SHOP",
+            'code' => rand(999999, 9999999),
+            'website_url' => Str::random(15) . ".com",
+        ];
+    }
+}
