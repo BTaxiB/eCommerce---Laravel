c1eacbd127a351aac664943fe74b3c4462951cf4
unknown <joksimovicmiki7@gmail.com>
2021-01-04 20:05:45 +0100

    minor changges

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -174,7 +174,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy(int $id)
