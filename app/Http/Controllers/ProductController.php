447695b782e4abedbae950604054e38a6f3d2cad
unknown <joksimovicmiki7@gmail.com>
2021-01-04 14:17:43 +0100

    Product and Store controllers, CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\Store;
+use App\Models\StoreCircuit;
 use App\Models\Url;
 use Illuminate\Http\Request;
 
@@ -16,7 +16,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $item = Product::all();
+
+        return view('/products', compact('item'));
     }
 
     /**
@@ -26,7 +28,7 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        return view('products.create');
     }
 
     /**
@@ -37,39 +39,58 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Product $product)
-    {
-        //
+        $request->validate([
+            'name' => 'required|max:200',
+            'description' => 'max:500',
+            'price' => 'required',
+        ]);
+
+        $product = new Product;
+        $product->name          = validInput($request->input('name'));
+        $product->description   = validInput($request->input('description'));
+        $product->price         = floatval($request->input('price'));
+        $product->save();
+
+        //Storing URL
+        if (checkUrl($request->input('product_url'))) {
+            $url = new Url;
+            $url->name = $request->input('product_url');
+            $product->url()->save($url);
+        }
+
+        //format selected option, extract store_id
+        $storeId = getStringBetween($request->input('store'), "y", "z");
+
+        $circuit = new StoreCircuit;
+        $circuit->product_id = $product->id;
+        $circuit->store_id = $storeId;
+        $circuit->save();
+
+        return view("/products");
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
+    public function edit(int $id)
     {
-        //
+        $item = Product::findOrFail($id);
+
+        return view("products.edit", compact('item'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, int $id)
     {
         //
     }
@@ -80,8 +101,11 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy(int $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return view('/products');
     }
 }
---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Store;
+use App\Models\Url;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $item = Store::all();
+
+        return view('/stores', compact('item'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('store.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:App\Models\Store|max:200',
+            'website_url' => 'required|unique:App\Models\Store',
+            'code' => 'required|unique:App\Models\Store',
+        ]);
+
+
+        $store              = new Store;
+        $store->name        = validInput($request->input('name'));
+        $store->website_url = $request->input('website_url');
+        $store->code        = $request->input('code');
+
+        $store->save();
+
+        return view('/stores');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $store
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(int $id)
+    {
+        $item = Store::findOrFail($id);
+
+        return view('stores.edit', compact('item'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $id)
+    {
+        $request->validate([
+            'name' => 'unique:App\Models\Store|max:200',
+            'website_url' => 'unique:App\Models\Store',
+            'code' => 'unique:App\Models\Store',
+        ]);
+
+        $store = Store::findOrFail($id);
+
+        if ($store) {
+            if ($request->hasInput('name')) {
+                $store->name = validInput($request->input('name'));
+            }
+
+            if ($request->hasInput('website_url')) {
+                $store->website_url = $request->input('website_url');
+            }
+
+            if ($request->hasInput('code')) {
+                $store->code = $request->input('code');
+            }
+
+            $store->save();
+
+            return view('/stores');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(int $id)
+    {
+        $store = Store::findOrFail($id);
+        $store->delete();
+
+        return redirect('/stores');
+    }
+}
